0355b05bc9b18a43817d48b7253c611a9cfa94dd
mianihsanshah <mianihsanshah@gmail.com>
2020-06-21 18:38:25 +0500

    testing surge

--- src/Child.js
+++ src/Child.js
@@ -7,7 +7,6 @@ const Child = () => {
     return(
         <div>
             <h2>This is First Child Component Using CounterContext Hook</h2>
-            <h3>Testing Surge</h3>
             <h4>Value of Context Counter is: {counterValue[0]}</h4>
             <button onClick={() => {counterValue[1](++counterValue[0])}}>Increament Context Counter</button>
             <button onClick={() => {counterValue[1](--counterValue[0])}}>Decreament Context Counter</button>
